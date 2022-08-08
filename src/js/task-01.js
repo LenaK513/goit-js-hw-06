ae2507cd79b961d80231bf839dd90bf0998a5da5
Olena <kryvtsovaolena@gmail.com>
2022-08-08 22:18:15 +0300

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,9 @@
+const quantityOfEl = document.querySelectorAll(".item");
+console.log("Number of categories:", quantityOfEl.length);
+
+quantityOfEl.forEach((element) => {
+  const elTitle = element.querySelector("h2");
+  const elLenght = element.querySelectorAll("li");
+  console.log(`Category:`, elTitle.innerHTML);
+  console.log(`Elements:`, elLenght.length);
+});
