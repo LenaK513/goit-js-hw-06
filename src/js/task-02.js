b98f1336070b6bdfc8318569d9b5600097d624b3
Olena <kryvtsovaolena@gmail.com>
2022-08-09 20:01:19 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,51 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+console.log(ingredients);
+const ingredientsItemEl = document.createElement("li");
+ingredientsItemEl.classList.add("item");
+ingredientsItemEl.textContent = "Potatoes";
+// console.log(ingredientsItemEl);
+
+const ingredientsItemElSecond = document.createElement("li");
+ingredientsItemElSecond.classList.add("item");
+ingredientsItemElSecond.textContent = "Mushrooms";
+// console.log(ingredientsItemElSecond);
+
+const ingredientsItemElThird = document.createElement("li");
+ingredientsItemElThird.classList.add("item");
+ingredientsItemElThird.textContent = "Garlic";
+// console.log(ingredientsItemElThird);
+
+const ingredientsItemElForth = document.createElement("li");
+ingredientsItemElForth.classList.add("item");
+ingredientsItemElForth.textContent = "Tomatos";
+// console.log(ingredientsItemElForth);
+
+const ingredientsItemElFifth = document.createElement("li");
+ingredientsItemElFifth.classList.add("item");
+ingredientsItemElFifth.textContent = "Herbs";
+// console.log(ingredientsItemElFifth);
+
+const ingredientsItemElSixth = document.createElement("li");
+ingredientsItemElSixth.classList.add("item");
+ingredientsItemElSixth.textContent = "Condiments";
+// console.log(ingredientsItemElSixth);
+
+const newIngredientsList = document.querySelector("#ingredients");
+
+newIngredientsList.append(
+  ingredientsItemEl,
+  ingredientsItemElSecond,
+  ingredientsItemElThird,
+  ingredientsItemElForth,
+  ingredientsItemElFifth,
+  ingredientsItemElSixth
+);
+
+console.log(newIngredientsList);
