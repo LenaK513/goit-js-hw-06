9e340268074877710573a9ec4b2e97a11ffc7528
Olena <kryvtsovaolena@gmail.com>
2022-08-11 23:08:54 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const input = document.querySelector("#name-input");
+      const span = document.querySelector("#name-output");
+
+      input.addEventListener("input", onInputChangeText);
+
+      function onInputChangeText(event) {
+        span.textContent = event.currentTarget.value;
