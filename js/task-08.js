c1f197be57c88226b5cdd7acf1fcf9845cc162d1
Olena <kryvtsovaolena@gmail.com>
2022-08-12 22:00:18 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const elementsOfForm = event.currentTarget.elements;
+  const email = elementsOfForm.email.value;
+  const password = elementsOfForm.password.value;
+
+  if (email === "" || password === "") {
+    alert("Fill the form!");
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+    form.reset();
+  }
+}
