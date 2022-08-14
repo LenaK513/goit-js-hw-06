bdedc3e4a37a0e35cdb447eace4f4436c95f14d1
Olena <kryvtsovaolena@gmail.com>
2022-08-15 00:50:37 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,20 @@
 const inputEl = document.querySelector("#validation-input");
 // console.dir(input);
-console.dir(inputEl.attributes[2].value);
+// console.dir(inputEl.attributes[2].value);
 
 inputEl.addEventListener("blur", onInputBlurCountSymbols);
 
 function onInputBlurCountSymbols(event) {
   let length = inputEl.value.length;
+  const lengthInitial = Number(inputEl.attributes[2].value);
   console.log(length);
+  console.log(lengthInitial);
 
-  if (length <= inputEl.attributes[2].value) {
+  if (length === lengthInitial) {
     this.classList.add("valid");
+    this.classList.remove("invalid");
   } else {
+    this.classList.remove("valid");
     this.classList.add("invalid");
   }
 }
