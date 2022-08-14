4496abb7583a106a843db8abc164d83174fb4b65
Olena <kryvtsovaolena@gmail.com>
2022-08-14 23:28:02 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,19 @@
 const input = document.querySelector("#name-input");
 const span = document.querySelector("#name-output");
 
-input.addEventListener("input", onInputChangeText);
-
 function onInputChangeText(event) {
-  span.textContent = event.currentTarget.value;
+  if (event.currentTarget.value.trim() === "") {
+    span.textContent = "Anonymous";
+  } else {
+    span.textContent = event.currentTarget.value;
+  }
 }
+
+input.addEventListener("input", onInputChangeText);
+// function onInputChangeText(event) {
+//   span.textContent = event.currentTarget.value;
+
+//   if (event.currentTarget.value === "") {
+//     span.textContent = "Anonymous";
+//   }
+// }
