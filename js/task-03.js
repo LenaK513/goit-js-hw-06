7750c98176102ac262877e96784c6634822b54f6
Olena <kryvtsovaolena@gmail.com>
2022-08-14 23:15:33 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -14,11 +14,26 @@ const images = [
 ];
 
 const gallery = document.querySelector(".gallery");
-const listOfElements = images.map((image) => {
-  gallery.insertAdjacentHTML(
-    "beforeend",
-    `<li> <img class="image-item" src=${image.url} alt=${image.alt} /> </li>`
-  );
-});
 
+const listOfElements = (image) => {
+  return `<li> <img class="image-item" src=${image.url} alt='${image.alt}' /> </li>`;
+};
+
+const listOfImages = images.map(listOfElements).join("");
+
+gallery.insertAdjacentHTML("beforeend", listOfImages);
 console.log(gallery);
+
+// const gallery = document.querySelector(".gallery");
+// const listOfElements = images.map((image) => {
+//   // const imageEl = document.createElement("img");
+//   // imageEl.src = image.url;
+//   // imageEl.alt = image.alt;
+//   // console.log(imageEl);
+//   gallery.insertAdjacentHTML(
+//     "beforeend",
+//     `<li> <img class="image-item" src=${image.url} alt=${image.alt} /> </li>`
+//   );
+// });
+
+// console.log(gallery);
